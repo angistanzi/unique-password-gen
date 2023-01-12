db1d9023992037ff8e93c3313e894b85a75e9797
Gini Stanzi <a.stanzione14@gmail.com>
2023-01-12 06:44:48 +0000

    updated functions to generate random character and build password

--- script.js
+++ script.js
@@ -88,40 +88,87 @@ let upperCasedCharacters = [
   'Z'
 ];
 
-let userOptions = []
+let userOptions = [];
 
+let upperLowerCharacters = lowerCasedCharacters.concat(upperCasedCharacters);
+let allCharacters =  specialCharacters.concat(numericCharacters.concat(lowerCasedCharacters.concat(upperCasedCharacters)));
+let specialUpperLowerCharacters = lowerCasedCharacters.concat(upperCasedCharacters.concat(specialCharacters));
+let numerUpperLowerCharacters = lowerCasedCharacters.concat(upperCasedCharacters.concat(numericCharacters));
+  
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  console.log(prompt("Please enter how many characters you would like your password to be. Enter a number between 10 and 64 only."));
-  console.log(confirm("Would you like lowercase characters? Select 'OK' for YES or 'Cancel' for NO."));
-  console.log(confirm("Would you like uppercase characters? Select 'OK' for YES or 'Cancel' for NO."));
-  return userOptions.push
-}
-console.log(userOptions)
 
-getPasswordOptions()
+  window.userOptionZero = prompt("Please enter how many characters you would like your password to be. Enter a number between 10 and 64 only.");
+  let userOptionOne = confirm("Would you like numeric characters in your password? Select 'OK' for YES, or 'Cancel' for NO.");
+  let userOptionTwo = confirm("Would you like special characters ($@%&*?, etc.) in your password? Select 'OK' for YES, or 'Cancel' for NO.");
+  
+  return userOptions.push(userOptionZero, userOptionOne, userOptionTwo);
+}
+console.log(userOptions);
+
+getPasswordOptions();
 
 // Function for getting a random element from an array
-function getRandom(arr) {
+function getRandom() {
 
-}
+  let randomAllCharacters = Math.floor(Math.random()*allCharacters.length);
+  let randomNumerUpperLowerCharacters = Math.floor(Math.random()*numerUpperLowerCharacters.length);
+  let randomSpecialUpperLowerCharacters = Math.floor(Math.random()*specialUpperLowerCharacters.length);
+  let randomUpperLowerCharacters = Math.floor(Math.random()*upperLowerCharacters.length);
+
+  if (userOptions[1]===true & userOptions[2]===true){
+  
+    return allCharacters[randomAllCharacters];
+
+  
+  } else if (userOptions[1]===true & userOptions[2]===false){
+  
+    return numerUpperLowerCharacters[randomNumerUpperLowerCharacters];
+  
+  } else if (userOptions[1]===false & userOptions[2]===true){
+
+  
+    return specialUpperLowerCharacters[randomSpecialUpperLowerCharacters];
+  
+  
+  }else if (userOptions[1]===false & userOptions[2]===false){
+    return upperLowerCharacters[randomUpperLowerCharacters];
+  
+  };
+
+
+
+};
+
+console.log(getRandom());
+
+
 
 // Function to generate password with user input
 function generatePassword() {
-
+let finalUserPass = ""
+for (let i = 0; i < window.userOptionZero; i++) {
+  
+ let userPass = getRandom()
+ finalUserPass += userPass
+  
 }
+  return finalUserPass
+};
+
+console.log(generatePassword());
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+let generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  let password = generatePassword();
+  let passwordText = document.querySelector('#password');
 
   passwordText.value = password;
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
